9274f5f677bf23d0316f6d354aa50b3c552bffe5
Luken Iriondo <lukeniri@gmail.com>
2026-07-21 00:02:42 +0200

    fix: update service worker cache version from v1 to v2

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Bump CACHE when assets change to invalidate.
    ============================================= */
 
-const CACHE = 'iamludok-v1';
+const CACHE = 'iamludok-v2';
 
 const CORE = [
   '/',
